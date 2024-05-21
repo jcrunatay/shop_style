d14a7ff2ef7164585316e9353cb066e7eff17a39
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-05-21 17:44:09 -0400

    Created intro text inside the video for desktop version

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,12 +1,49 @@
+import { useState, useEffect } from "react";
+
+import Collections from "../../components/collections/collections.component";
+import ExpandedCollection from "../../components/expanded-collections/expanded-collections.component";
+
 import shopstylevideo from "../../assets/hero-video.mp4";
 import "./home.styles.scss";
-import Collections from "../../components/collections/collections.component";
 const Home = () => {
+    //use screenwidth for the id ref of shop now button
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
         <section className="homepage">
-            <video style={{ width: "100%" }} src={shopstylevideo} loop muted autoPlay>
-                Your browser does not support the video tag.
-            </video>
+            <div className="video-container">
+                <div className="intro-context-in-video">
+                    <h1 className="intro-title">Discover the Latest Trends</h1>
+                    <p className="intro-text">
+                        Welcome to our exclusive collection of trendy apparel and accessories,
+                        carefully curated to keep you stylish and on-trend all year round. Discover
+                        the latest trends, mix and match styles, and create the perfect look for any
+                        occasion. Step into a world of fashion and discover endless possibilities
+                        with our carefully selected pieces. Elevate your style, elevate your
+                        wardrobe, and elevate your confidence with our collection.
+                    </p>
+                    <a
+                        href={`${screenWidth < 1024 ? "#collections" : "#expanded-collections"}`}
+                        className="shop-now-button"
+                    >
+                        Shop Now
+                    </a>
+                </div>
+                <video style={{ width: "100%" }} src={shopstylevideo} loop muted autoPlay>
+                    Your browser does not support the video tag.
+                </video>
+            </div>
             <div className="homepage-intro-container">
                 <h1 className="intro-title">Discover the Latest Trends</h1>
                 {/* <p className="intro-text">
@@ -27,6 +64,7 @@ const Home = () => {
                 </a>
             </div>
             <Collections />
+            <ExpandedCollection />
         </section>
     );
 };
