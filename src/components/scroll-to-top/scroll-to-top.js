ac7b6d04581f3ecc5a479a4978f0f8fa54c7f7ed
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-05-21 17:48:33 -0400

    added scroll to top whenever page changes

--- src/components/scroll-to-top/scroll-to-top.js
+++ src/components/scroll-to-top/scroll-to-top.js
@@ -3,7 +3,6 @@ import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
     const { pathname } = useLocation();
-    console.log("🚀 ~ ScrollToTop ~ useLocation:", useLocation());
 
     useEffect(() => {
         window.scrollTo(0, 0);
