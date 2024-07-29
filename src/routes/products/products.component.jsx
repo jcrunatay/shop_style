5e88023ef1d1c1b39be2b6c686e730e3dadcab54
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-07-29 13:45:26 -0400

    created a new route to redirect user to single product category only

--- src/routes/products/products.component.jsx
+++ src/routes/products/products.component.jsx
@@ -1,25 +1,16 @@
-import Collections from "./../../components/collections/collections.component";
-import "./products.styles.scss";
-/* import { useEffect } from "react";
- */ const Products = () => {
-    /* useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []); */
+import { Routes, Route } from "react-router-dom";
+import ProductsCollectionPreview from "../../components/products-collection-preview/products-collection-preview.component";
+import SingleCollectionProducts from "../single-collection-products/single-collection-products.component";
+
+const Products = () => {
     return (
-        <section id="products">
-            <div className="products-context-container">
-                <h2 className="page-title">PRODUCTS</h2>
-                <h3 className="about-products-heading">What We Offer</h3>
-                <p className="about-products-text">
-                    At ShopStyle, we believe in providing not just products, but experiences that
-                    enhance your lifestyle. Dive into our exclusive collection where every item
-                    tells a story of quality and style. Whether you're in search of the latest in
-                    tech innovations or the trendiest additions to your wardrobe, our curated
-                    selections are designed to cater to every taste and need.
-                </p>
-            </div>
-            <Collections />
-        </section>
+        <Routes>
+            <Route
+                index
+                element={<ProductsCollectionPreview renderedInProductsComponent={true} />}
+            />
+            <Route path=":category/*" element={<SingleCollectionProducts />} />
+        </Routes>
     );
 };
 
