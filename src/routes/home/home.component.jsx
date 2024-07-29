ca04fc258a0eee17440cb83d8bb2feebe41d3eb0
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-07-29 13:43:09 -0400

    added the collections preview and the expanded collection component

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 
+import ExpandedCollections from "../../components/expanded-collections/expanded-collections.component";
 import Collections from "../../components/collections/collections.component";
-import ExpandedCollection from "../../components/expanded-collections/expanded-collections.component";
 
 import shopstylevideo from "../../assets/hero-video.mp4";
 import "./home.styles.scss";
+
 const Home = () => {
-    //use screenwidth for the id ref of shop now button
     const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
     useEffect(() => {
@@ -46,11 +46,6 @@ const Home = () => {
             </div>
             <div className="homepage-intro-container">
                 <h1 className="intro-title">Discover the Latest Trends</h1>
-                {/* <p className="intro-text">
-                Explore our exclusive collection of trendy apparel and accessories, curated to keep
-                you stylish all year round. From timeless classics to the hottest new arrivals, find
-                everything you need to elevate your wardrobe.
-            </p> */}
                 <p className="intro-text">
                     Welcome to our exclusive collection of trendy apparel and accessories, carefully
                     curated to keep you stylish and on-trend all year round. Discover the latest
@@ -59,12 +54,15 @@ const Home = () => {
                     selected pieces. Elevate your style, elevate your wardrobe, and elevate your
                     confidence with our collection.
                 </p>
-                <a href="#collections" className="shop-now-button">
+                <a
+                    href={`${screenWidth < 1024 ? "#collections" : "#expanded-collections"}`}
+                    className="shop-now-button"
+                >
                     Shop Now
                 </a>
             </div>
-            <Collections />
-            <ExpandedCollection />
+            <Collections renderedInProductsComponent={false} />
+            <ExpandedCollections />
         </section>
     );
 };
