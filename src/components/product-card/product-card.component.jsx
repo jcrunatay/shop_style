3e824258d130512ccf6844aae2323a18cbd0bca0
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-07-29 13:36:34 -0400

    added product card

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,43 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+import "./product-card.styles.scss";
+
+const ProductCard = ({ product }) => {
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
+
+    const navigate = useNavigate();
+    const { category } = useParams();
+    const { id, title, price, description, image } = product;
+
+    const redirectUserToSingleProductPage = () => {
+        navigate(`/products/${category}/${id}`);
+    };
+
+    return (
+        <div className="card">
+            <div className="card-upper-content">
+                <img loading="lazy" src={image} alt={title} />
+            </div>
+            <div className="card-lower-content">
+                <div className="product-info-container">
+                    <span className="product-name">{title}</span>
+                    <span className="product-description">{description}</span>
+                    <span className="product-price">${price}</span>
+                </div>
+                <div className="btns-container">
+                    <button className="add-to-cart" onClick={addProductToCart}>
+                        Add to Cart
+                    </button>
+                    <button className="view-item" onClick={redirectUserToSingleProductPage}>
+                        View Item
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
