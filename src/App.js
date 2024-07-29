b2890b271d3d8b90f6e922a4fd87a07351a2ca1f
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-07-29 13:09:18 -0400

    added checkout and authentication routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import About from "./routes/about/about.component";
 import Home from "./routes/home/home.component";
 import Products from "./routes/products/products.component";
 import Contact from "./routes/contact/contact.component";
+import CheckOut from "./routes/checkout/checkout.component";
+import Authentication from "./routes/authentication/authentication.component";
 import ScrollToTop from "./components/scroll-to-top/scroll-to-top";
 
 import { Routes, Route } from "react-router-dom";
@@ -14,8 +16,10 @@ const App = () => {
                 <Route path="/" element={<Navigation />}>
                     <Route index element={<Home />} />
                     <Route path="about" element={<About />} />
-                    <Route path="products" element={<Products />} />
+                    <Route path="products/*" element={<Products />} />
                     <Route path="contact" element={<Contact />} />
+                    <Route path="checkout" element={<CheckOut />} />
+                    <Route path="auth/*" element={<Authentication />} />
                 </Route>
             </Routes>
         </div>
