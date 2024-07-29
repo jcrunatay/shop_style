637772c42f9ce07ead87c37833d32b53ef831589
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-07-29 13:40:51 -0400

    added user and cart provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
+import { UserProvider } from "./context/user.context";
+import { CartProvider } from "./context/cart.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <CartProvider>
+                <UserProvider>
+                    <App />
+                </UserProvider>
+            </CartProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
