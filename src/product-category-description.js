fcae1d805d647a9af440a4fced307ba28a225704
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-07-29 13:41:13 -0400

    added array of product names and description

---
+++ src/product-category-description.js
@@ -0,0 +1,24 @@
+const ProductCollectionTitleAndDescription = [
+    {
+        title: "women's clothing",
+        description:
+            "Dress up in elegance with our extensive range of dresses, tops, and skirts that are perfect for any occasion.",
+    },
+    {
+        title: "men's clothing",
+        description:
+            "From formal suits that make an impression to casual wear that comforts, find your unique style statement here.",
+    },
+    {
+        title: "jewelry",
+        description:
+            "Add a sparkle to your everyday look with our exquisite collection ofjewelry. Choose from classic pieces to contemporary designs that suityour style.",
+    },
+    {
+        title: "electronics",
+        description:
+            "Stay up-to-date with the latest gadgets and tech essentials that are a must for your digital lifestyle.",
+    },
+];
+
+export default ProductCollectionTitleAndDescription;
