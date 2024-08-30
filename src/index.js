2ab3455d1287b8e5934a184ba82d8567ef013be4
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-08-29 20:10:19 -0400

    removed user and cart provider and added New Provider from react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
+
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 import "./index.scss";
 import { UserProvider } from "./context/user.context";
 import { CartProvider } from "./context/cart.context";
@@ -9,12 +14,14 @@ import { CartProvider } from "./context/cart.context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <CartProvider>
-                <UserProvider>
-                    <App />
-                </UserProvider>
-            </CartProvider>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                {/* <CartProvider> */}
+                {/* <UserProvider> */}
+                <App />
+                {/* </UserProvider> */}
+                {/* </CartProvider> */}
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
