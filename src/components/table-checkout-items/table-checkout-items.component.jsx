793f67e5821b209b076a5cc639401529f162ce60
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-08-29 20:11:55 -0400

    removed user and cart context and used useSelector to get cartItems

--- src/components/table-checkout-items/table-checkout-items.component.jsx
+++ src/components/table-checkout-items/table-checkout-items.component.jsx
@@ -1,11 +1,14 @@
-import { useContext } from "react";
-import { CartContext } from "../../context/cart.context";
+/* import { useContext } from "react";
+import { CartContext } from "../../context/cart.context"; */
+
+import { useSelector } from "react-redux";
+import { selectCartItems } from "../../store/cart/cart.selector";
 
 import TableCheckoutItem from "../table-checkout-item/table-checkout-item.component";
 import "./table-checkout-items.styles.scss";
 
 const TableCheckoutItems = () => {
-    const { cartItems } = useContext(CartContext);
+    const cartItems = useSelector(selectCartItems);
 
     const checkOutItems = cartItems.map((cartItem) => (
         <TableCheckoutItem product={cartItem} key={cartItem.id} />
