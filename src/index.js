736b503ca3f7cd266aef1ff0b24753ca0cda1910
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-10-07 13:16:32 -0400

    updated so store would persist

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
+
+import { PersistGate } from "redux-persist/integration/react";
+
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+
+import { store, persistor } from "./store/store";
 
 import "./index.scss";
+
 import { UserProvider } from "./context/user.context";
 import { CartProvider } from "./context/cart.context";
 
@@ -15,13 +20,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                {/* <CartProvider> */}
-                {/* <UserProvider> */}
-                <App />
-                {/* </UserProvider> */}
-                {/* </CartProvider> */}
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
