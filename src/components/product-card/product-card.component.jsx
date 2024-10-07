085df09431183ea31e9b5eabf73c3150d1ff9ad8
Juyan Criston Runatay <juyanrunatay@gmail.com>
2024-10-07 13:18:01 -0400

    updated file path reference from cart.action to cart.reducer

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,8 @@
 import { useNavigate, useParams } from "react-router-dom";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-import { selectCartItems } from "../../store/cart/cart.selector";
-import { addItemToCart } from "../../store/cart/cart.action";
+import { addItemToCart } from "../../store/cart/cart.reducer";
 
 /* import { useContext } from "react";
 import { CartContext } from "../../context/cart.context"; */
@@ -11,8 +10,8 @@ import "./product-card.styles.scss";
 
 const ProductCard = ({ product }) => {
     const dispatch = useDispatch();
-    const cartItems = useSelector(selectCartItems);
-    const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
+
+    const addProductToCart = () => dispatch(addItemToCart(product));
 
     const navigate = useNavigate();
     const { category } = useParams();
